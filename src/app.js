21af293c6c1f7bff3ba8d71132c3886e7456f1e4
Leonardo A <brinfo.leo@gmail.com>
2023-03-28 12:07:54 +0100

    separation of rules

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
-const mysql = require("mysql2");
-require('dotenv').config({ path: "./src/.env" })
-//console.log(process.env);
-const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+const dbConnect= require('./dbConnect')
 
-connection.connect((err)=> {
+dbConnect.connect((err)=> {
     if (err) throw err;
-    connection.query("SELECT * FROM users",  (err, result)=> {
+    dbConnect.query("SELECT * FROM users",  (err, result)=> {
       if (err) throw err;
       console.log(result);
     });
